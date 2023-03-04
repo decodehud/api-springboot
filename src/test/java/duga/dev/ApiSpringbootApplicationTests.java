b65b74a9534af5b94e44c2dfce17981d5ebd5583
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-03-03 20:46:25 -0600

    first commit

---
+++ src/test/java/duga/dev/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package duga.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
